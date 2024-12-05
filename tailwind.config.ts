effa0c782691053c9bc798d64c19c6ec5480c85e
Júlia Delisposte <loretta_deli@hotmail.com>
2024-12-05 16:20:34 -0300

    Chat GPT Clone

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'gpt-gray': '#343541',
+        'gpt-lightgray': '#40414f'
       },
+      keyframes: {
+        blink: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      },
+      animation: {
+        blink: 'blink 1s infinite'
+      }
     },
   },
   plugins: [],
